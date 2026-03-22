36a809a654d4580a873f0c621ab65fee63d55232
dom <dchwirot01@gmail.com>
2026-03-22 18:11:52 +0000

    ready for rel v1.1.1

--- Formula/sealsay.rb
+++ Formula/sealsay.rb
@@ -2,7 +2,7 @@ class Sealsay < Formula
   desc "CLI app that generates ASCII art of a seal saying a message"
   homepage "https://github.com/phantypengy/sealsay"
   url "https://github.com/phantypengy/sealsay/archive/v1.1.1.tar.gz"
-  sha256 "eae7f28e4afd412f6e9c62f5cb98d7c797283348ce763229ee600505c6d72cf7"
+  sha256 "e8e37fc2e3076d883d8cde1aebc63aa5fe9eec284138a153af0245fc6e526a66"
   license "GPL-3.0-or-later"
 
   depends_on "python3"
